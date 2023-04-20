ca617d44ba817c34bf6e69a1a6f5231989d0d116
Julian <118623124+julian-bloennigen@users.noreply.github.com>
2023-04-20 20:26:02 +0200

    lol

--- Tetris/app.py
+++ Tetris/app.py
@@ -1,13 +1,42 @@
+from PyQt6.QtWidgets import *
+
 import sys
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 class MainWindow(QMainWindow):
-
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
+        self.setWindowTitle("Tetris Game by Julian")
 
+        layout = QVBoxLayout()
+
+        start_button = QPushButton("Start Game")
+        start_button.clicked.connect(self.start_button_clicked)
+        settings_button = QPushButton("Settings")
+        settings_button.clicked.connect(self.settings_button_clicked)
+        exit_button = QPushButton("Exit")
+        exit_button.clicked.connect(self.exit_button_clicked)
+
+        layout.addWidget(start_button)
+        layout.addWidget(settings_button)
+        layout.addWidget(exit_button)
+
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+    
+    def start_button_clicked(self):
+        print("Starting Game!")
+        self.setEnabled(False)
+    
+    def settings_button_clicked(self):
+        print("Starting Game!")
+        self.setEnabled(False)
+
+    def exit_button_clicked(self):
+        print("Ending Game!")
+        self.setEnabled(False)
 
 app = QApplication(sys.argv)
 
@@ -15,4 +44,5 @@ window = MainWindow()
 window.show()
 
 app.exec()
+
 
