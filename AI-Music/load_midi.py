fca2a15e9d93c76293a337f7a3066208b903d4b4
Julian <118623124+julian-bloennigen@users.noreply.github.com>
2023-04-24 17:47:16 +0200

    ss

--- AI-Music/load_midi.py
+++ AI-Music/load_midi.py
@@ -1,7 +1,7 @@
 from music21 import converter, chord, note
 
-dataset_name = "cello"
-filename = "cs1-2all"
-file = "./data/{}/{}.mid".format(dataset_name, filename)
+dataset_name = "Bach"
+filename = "vs1-1ada"
+file = "data/Bach/vs1-1ada.mid".format()
 
 original_score = converter.parse(file).chordify()
